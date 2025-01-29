9b26c35bff553e47e21226ad863f1f2053ac6b66
Rueslan <krus1501@mail.ru>
2025-01-29 16:10:45 +0400

    Первый контроллер

---
+++ WebStore9/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore9.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => Content("First Controller");
+
+        public IActionResult SomeAction(int id) => Content($"First Controller {id}");
+    }
+}
