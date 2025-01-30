52440a07ffd2487cf0984b632c852ecfa7271f0d
Rueslan <krus1501@mail.ru>
2025-01-30 13:15:35 +0400

    Переработка  тестовых данных

--- WebStore9/Controllers/EmployeesController.cs
+++ WebStore9/Controllers/EmployeesController.cs
@@ -1,19 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore9.Data;
 using WebStore9.Models;
 
 namespace WebStore9.Controllers
 {
     public class EmployeesController : Controller
     {
-        private static readonly List<Employee> _Employees = new()
-        {
-            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 29, Seniority = 1 },
-            new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 35, Seniority = 4 },
-            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидр", Patronymic = "Сидорович", Age = 44, Seniority = 6 }
-        };
+        private readonly IEnumerable<Employee> _Employees;
+
+        public EmployeesController() => _Employees = TestData.Employees;
 
         public IActionResult Index() => View(_Employees);
 
-        public IActionResult EmployeeDetails(int id) => View(_Employees[id - 1]);
+        //public IActionResult EmployeeDetails(int id) => View(_Employees[id - 1]);
     }
 }
