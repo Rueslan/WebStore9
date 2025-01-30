e6143c553a8abc014fca12efc3535a6b62f6ddf3
Rueslan <krus1501@mail.ru>
2025-01-30 13:06:23 +0400

    Выделение работы с сотрудниками в отдельный контроллер

---
+++ WebStore9/Controllers/EmployeesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore9.Models;
+
+namespace WebStore9.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private static readonly List<Employee> _Employees = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 29, Seniority = 1 },
+            new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 35, Seniority = 4 },
+            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидр", Patronymic = "Сидорович", Age = 44, Seniority = 6 }
+        };
+
+        public IActionResult Index() => View(_Employees);
+
+        public IActionResult EmployeeDetails(int id) => View(_Employees[id - 1]);
+    }
+}
--- WebStore9/Controllers/HomeController.cs
+++ WebStore9/Controllers/HomeController.cs
@@ -1,23 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using WebStore9.Models;
 
 namespace WebStore9.Controllers
 {
     public class HomeController : Controller
     {
-
-        private static readonly List<Employee> _Employees = new()
-        {
-            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 29, Seniority = 1 },
-            new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 35, Seniority = 4 },
-            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидр", Patronymic = "Сидорович", Age = 44, Seniority = 6 }
-        };
         public IActionResult Index() => View();
 
-        public IActionResult SomeAction(int id) => Content($"First Controller {id}");
-
-        public IActionResult Employees() => View(_Employees);
-
-        public IActionResult EmployeeDetails(int id) => View(_Employees[id - 1]);
     }
 }
