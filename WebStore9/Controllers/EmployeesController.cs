c2a91704f12c9400f249b8ec221625f2993d0783
Rueslan <krus1501@mail.ru>
2025-01-31 10:17:53 +0400

    Добавление возможности удаления сотрудника

--- WebStore9/Controllers/EmployeesController.cs
+++ WebStore9/Controllers/EmployeesController.cs
@@ -6,20 +6,24 @@ namespace WebStore9.Controllers
 {
     public class EmployeesController : Controller
     {
-        private readonly IEnumerable<Employee> _Employees;
-
-        public EmployeesController() => _Employees = TestData.Employees;
-
-        public IActionResult Index() => View(_Employees);
+        public IActionResult Index() => View(TestData.Employees);
 
         public IActionResult Details(int id)
         {
-            var employee = _Employees.SingleOrDefault(e => e.Id == id);
+            var employee = TestData.Employees.SingleOrDefault(e => e.Id == id);
 
             if (employee == null)
                 return NotFound();
 
             return View(employee);
         }
+
+        public IActionResult EmployeeDelete(int id)
+        {
+            TestData.Employees.Remove(TestData.Employees.SingleOrDefault(e => e.Id == id));
+
+            return View("Index", TestData.Employees);
+        }
+
     }
 }
--- WebStore9/Data/TestData.cs
+++ WebStore9/Data/TestData.cs
@@ -4,7 +4,7 @@ namespace WebStore9.Data
 {
     public static class TestData
     {
-        public static List<Employee> Employees { get; } = new() 
+        public static List<Employee> Employees { get; set; } = new() 
         {
             new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 29, Seniority = 1 },
             new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 35, Seniority = 4 },
