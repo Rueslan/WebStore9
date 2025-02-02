028a2b343d03fa2a8739b79f059b17abe85e6394
Rueslan <krus1501@mail.ru>
2025-02-02 16:03:31 +0400

    Визуальные аннотации данных модели-представления

--- WebStore9/ViewModels/EmployeeViewModel.cs
+++ WebStore9/ViewModels/EmployeeViewModel.cs
@@ -1,12 +1,26 @@
-﻿namespace WebStore9.ViewModels
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebStore9.ViewModels
 {
     public class EmployeeViewModel
     {
+        [HiddenInput(DisplayValue = false)]
         public int Id { get; set; }
+
+        [Display(Name = "Имя")]
         public string Name { get; set; }
+
+        [Display(Name = "Фамилия")]
         public string LastName { get; set; }
+
+        [Display(Name = "Отчество")]
         public string Patronymic { get; set; }
+
+        [Display(Name = "Возраст")]
         public int Age { get; set; }
+
+        [Display(Name = "Опыт")]
         public int Seniority { get; set; }
     }
 }
