8930f06ea794a4784a674ac1a50f1a0bb1c0cb26
Rueslan <krus1501@mail.ru>
2025-02-02 08:53:45 +0400

    Интерфейс сервиса сотрудников

---
+++ WebStore9/Services/Interfaces/IEmplyeesData.cs
@@ -0,0 +1,17 @@
+﻿using WebStore9.Models;
+
+namespace WebStore9.Services.Interfaces
+{
+    public interface IEmplyeesData
+    {
+        IEnumerable<Employee> GetAll();
+
+        Employee GetById(int id);
+
+        int Add(Employee employee);
+
+        void Update(Employee employee);
+
+        bool Delete(int id);
+    }
+}
