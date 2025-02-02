7df25aa6e36e32c18d3c9fddc0862b471342c729
Rueslan <krus1501@mail.ru>
2025-02-02 18:41:20 +0400

    Заготовки визуальных компонент

---
+++ WebStore9/Components/BrandsViewComponent.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore9.Components
+{
+    public class BrandsViewComponent: ViewComponent
+    {
+        public IViewComponentResult Invoke() => View();
+    }
+}
