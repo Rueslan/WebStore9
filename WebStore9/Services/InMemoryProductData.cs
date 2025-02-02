17fab86139748367e0d9d15314a26da12e3c89fd
Rueslan <krus1501@mail.ru>
2025-02-02 18:26:01 +0400

    Сервис каталога товаров и  тестовые данные

---
+++ WebStore9/Services/InMemoryProductData.cs
@@ -0,0 +1,19 @@
+﻿using WebStore9.Data;
+using WebStore9.Services.Interfaces;
+using WebStore9Domain.Entities;
+
+namespace WebStore9.Services
+{
+    public class InMemoryProductData : IProductData
+    {
+        public IEnumerable<Brand> GetBrands()
+        {
+            return TestData.Brands;
+        }
+
+        public IEnumerable<Section> GetSections()
+        {
+            return TestData.Sections;
+        }
+    }
+}
---
+++ WebStore9/Services/Interfaces/IProductData.cs
@@ -0,0 +1,11 @@
+﻿using WebStore9Domain.Entities;
+
+namespace WebStore9.Services.Interfaces
+{
+    public interface IProductData
+    {
+        IEnumerable<Section> GetSections();
+
+        IEnumerable<Brand> GetBrands();
+    }
+}
