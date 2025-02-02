7869e34a268d2f97b075e6f89ebcbef9b33d0bbb
Rueslan <krus1501@mail.ru>
2025-02-02 17:50:47 +0400

    Сущности бренда и секции

---
+++ WebStore9Domain/Entities/Brand.cs
@@ -0,0 +1,10 @@
+﻿using WebStore9Domain.Entities.Base;
+using WebStore9Domain.Entities.Base.Interfaces;
+
+namespace WebStore9Domain.Entities
+{
+    public class Brand : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+    }
+}
---
+++ WebStore9Domain/Entities/Section.cs
@@ -0,0 +1,12 @@
+﻿using WebStore9Domain.Entities.Base;
+using WebStore9Domain.Entities.Base.Interfaces;
+
+namespace WebStore9Domain.Entities
+{
+    public class Section : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+
+        public int? ParentId { get; set; }
+    }
+}
