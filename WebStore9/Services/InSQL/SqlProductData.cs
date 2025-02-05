3ffc0db8684cbc50bd9c7b5f619dc0c4f06f3d5c
Rueslan <krus1501@mail.ru>
2025-02-05 17:43:12 +0400

    Сервис каталога товаров в БД

---
+++ WebStore9/Services/InSQL/SqlProductData.cs
@@ -0,0 +1,40 @@
+﻿using WebStore9.DAL.Context;
+using WebStore9.Data;
+using WebStore9.Services.Interfaces;
+using WebStore9Domain;
+using WebStore9Domain.Entities;
+
+namespace WebStore9.Services.InSQL
+{
+    public class SqlProductData : IProductData
+    {
+        private readonly WebStore9DB _db;
+
+        public SqlProductData(WebStore9DB db)
+        {
+            _db = db;
+        }
+        public IEnumerable<Section> GetSections()
+        {
+            return _db.Sections;
+        }
+
+        public IEnumerable<Brand> GetBrands()
+        {
+            return _db.Brands;
+        }
+
+        public IEnumerable<Product> GetProducts(ProductFilter Filter = null)
+        {
+            IQueryable<Product> query = _db.Products;
+
+            if (Filter?.SectionId != null)
+                query = query.Where(p => p.SectionId == Filter.SectionId);
+
+            if (Filter?.BrandId != null)
+                query = query.Where(p => p.BrandId == Filter.BrandId);
+
+            return query;
+        }
+    }
+}
