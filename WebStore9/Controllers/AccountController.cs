c7e12e770ea5ba3479f02afd9a0e8cbd126c42b1
Rueslan <krus1501@mail.ru>
2025-02-06 14:42:32 +0400

    Заготовка контроллера Account

---
+++ WebStore9/Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore9.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register() => View();
+
+        public IActionResult Login() => View();
+
+        public IActionResult Logout() => RedirectToAction("Index","Home");
+
+        public IActionResult AccessDenied() => View();
+    }
+}
