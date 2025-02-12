d76b5abd676618441a99c5c4b90caecae7eaf913
Rueslan <krus1501@mail.ru>
2025-02-12 15:54:16 +0400

    Описание интерфейса сервиса корзины

---
+++ WebStore9/Services/Interfaces/ICartService.cs
@@ -0,0 +1,18 @@
+﻿using WebStore9.ViewModels;
+
+namespace WebStore9.Services.Interfaces
+{
+    public interface ICartService
+    {
+        void Add(int Id);
+
+        void Decrement(int Id);
+
+        void Remove(int Id);
+
+        void Clear();
+
+        CartViewModel GetViewModel();
+
+    }
+}
---
+++ WebStore9Domain/Entities/Cart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore9Domain.Entities
+{
+    public class Cart
+    {
+        public ICollection<CartItem> items { get; set; } = new List<CartItem>();
+
+        public int ItemsCount => items?.Sum(item => item.Quantity) ?? 0;
+
+    }
+
+    public class CartItem
+    {
+        public int ProductId { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
