08374455f13a5ff10eab744a6f3ec1c3553d7958
Rueslan <krus1501@mail.ru>
2025-02-12 16:54:25 +0400

    Модель-представления корзины

--- WebStore9/ViewModels/CartViewModel.cs
+++ WebStore9/ViewModels/CartViewModel.cs
@@ -2,5 +2,11 @@
 {
     public class CartViewModel
     {
+        public IEnumerable<(ProductViewModel Product, int Quantity)> Items { get; set; }
+
+        public int ItemsCount => Items?.Sum(i => i.Quantity) ?? 0;
+
+        public decimal TotalPrice => Items?.Sum(i => i.Product.Price * i.Quantity) ?? 0m;
+
     }
 }
