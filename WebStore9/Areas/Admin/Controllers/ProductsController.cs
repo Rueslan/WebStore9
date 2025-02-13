77874c0674d382d108eeeff06a44f8d4b9b3f30c
Rueslan <krus1501@mail.ru>
2025-02-13 16:15:34 +0400

    Список товаров в контроллере управления каталогом

--- WebStore9/Areas/Admin/Controllers/ProductsController.cs
+++ WebStore9/Areas/Admin/Controllers/ProductsController.cs
@@ -1,13 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore9.Services.Interfaces;
 
 namespace WebStore9.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class ProductsController : Controller
     {
+        private readonly IProductData _productData;
+
+        public ProductsController(IProductData ProductData)
+        {
+            _productData = ProductData;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var products = _productData.GetProducts();
+
+            return View(products);
         }
     }
 }
