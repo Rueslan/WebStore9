ac91240365a74da3cc0c2a4fa602db62c117819e
Rueslan <krus1501@mail.ru>
2025-02-16 11:34:55 +0400

    Интерфейс сервиса заказов

--- WebStore9/Services/Interfaces/IOrderService.cs
+++ WebStore9/Services/Interfaces/IOrderService.cs
@@ -1,6 +1,14 @@
-﻿namespace WebStore9.Services.Interfaces
+﻿using WebStore9.ViewModels;
+using WebStore9Domain.Entities.Orders;
+
+namespace WebStore9.Services.Interfaces
 {
     public interface IOrderService
     {
+        Task<IEnumerable<Order>> GetUserOrders(string user);
+
+        Task<Order> GetOrderById(int id);
+
+        Task<Order> CreateOrder(string userName, CartViewModel cart, OrderViewModel orderModel);
     }
 }
