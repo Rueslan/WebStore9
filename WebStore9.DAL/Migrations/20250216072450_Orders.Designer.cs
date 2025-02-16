0b09cd7d2d7f893b44e5d1dd6f5d95927ca568a2
Rueslan <krus1501@mail.ru>
2025-02-16 11:27:45 +0400

    Миграция БД для заказов

--- WebStore9.DAL/Migrations/20250216072450_Orders.Designer.cs
+++ WebStore9.DAL/Migrations/20250216072450_Orders.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebStore9.DAL.Context;
 
@@ -11,9 +12,11 @@ using WebStore9.DAL.Context;
 namespace WebStore9.DAL.Migrations
 {
     [DbContext(typeof(WebStore9DB))]
-    partial class WebStore9DBModelSnapshot : ModelSnapshot
+    [Migration("20250216072450_Orders")]
+    partial class Orders
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
