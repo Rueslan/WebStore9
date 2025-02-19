0b91eda015d5abf055a9ba12c530943e692f875a
Rueslan <krus1501@mail.ru>
2025-02-19 17:51:15 +0400

    Представления для контроллера профиля пользователя

---
+++ WebStore9/ViewModels/UserOrderViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace WebStore9.ViewModels
+{
+    public class UserOrderViewModel
+    {
+        public int Id { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Address { get; set; }
+
+        public decimal TotalPrice { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTimeOffset Date { get; set; }
+    }
+}
