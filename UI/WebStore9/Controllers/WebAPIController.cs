b4e2123c14e071fbaeb8c544174e88514db4f2a1
Rueslan <krus1501@mail.ru>
2025-02-24 11:43:24 +0400

    Визуализация данных, полученных от WebAPI

---
+++ UI/WebStore9/Controllers/WebAPIController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore9.Interfaces.TestAPI;
+
+namespace WebStore9.Controllers
+{
+    public class WebAPIController : Controller
+    {
+        private readonly IValuesService _valuesService;
+
+        public WebAPIController(IValuesService valuesService)
+        {
+            _valuesService = valuesService;
+        }
+        public IActionResult Index()
+        {
+            var values = _valuesService.GetAll();
+            return View(values);
+        }
+    }
+}
