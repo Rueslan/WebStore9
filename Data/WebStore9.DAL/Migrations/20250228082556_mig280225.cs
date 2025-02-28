6e73ba6af0da4dd6dd58eb8eb180ad1581375efd
Rueslan <krus1501@mail.ru>
2025-02-28 13:19:32 +0400

    Непонятки и исправление миграций

---
+++ Data/WebStore9.DAL/Migrations/20250228082556_mig280225.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebStore9.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig280225 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Decription",
+                table: "Orders",
+                newName: "Description");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Orders",
+                newName: "Decription");
+        }
+    }
+}
