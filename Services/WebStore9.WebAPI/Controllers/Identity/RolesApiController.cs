6fa87b72d49547080828a0dd55a08573b042b0e7
Rueslan <krus1501@mail.ru>
2025-02-28 15:32:53 +0400

    Заготовка логики контроллеров

--- Services/WebStore9.WebAPI/Controllers/Identity/RolesApiController.cs
+++ Services/WebStore9.WebAPI/Controllers/Identity/RolesApiController.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebStore9.DAL.Context;
 using WebStore9.Interfaces;
+using WebStore9Domain.Entities.Identity;
 
 namespace WebStore9.WebAPI.Controllers.Identity
 {
@@ -8,5 +13,14 @@ namespace WebStore9.WebAPI.Controllers.Identity
     [ApiController]
     public class RolesApiController : ControllerBase
     {
+        private readonly RoleStore<Role> _roleStore;
+        public RolesApiController(WebStore9DB db)
+        {
+            _roleStore = new(db);
+        }
+
+        [HttpGet("all")]
+        public async Task<IEnumerable<Role>> GetAll() => await _roleStore.Roles.ToArrayAsync();
+
     }
 }
--- Services/WebStore9.WebAPI/Controllers/Identity/UsersApiController.cs
+++ Services/WebStore9.WebAPI/Controllers/Identity/UsersApiController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebStore9.DAL.Context;
 using WebStore9.Interfaces;
+using WebStore9Domain.Entities.Identity;
 
 namespace WebStore9.WebAPI.Controllers.Identity
 {
@@ -8,5 +12,14 @@ namespace WebStore9.WebAPI.Controllers.Identity
     [ApiController]
     public class UsersApiController : ControllerBase
     {
+        private readonly UserStore<User, Role, WebStore9DB> _userStore;
+
+        public UsersApiController(WebStore9DB db)
+        {
+            _userStore = new (db);
+        }
+
+        [HttpGet("all")]
+        public async Task<IEnumerable<User>> GetAll() => await _userStore.Users.ToArrayAsync();
     }
 }
