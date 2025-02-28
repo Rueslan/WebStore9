c1f78caa16ec55b2e9928e6efee0ad98af285c83
Rueslan <krus1501@mail.ru>
2025-02-28 14:17:28 +0400

    Заготовки контроллеров для Identity

---
+++ Services/WebStore9.WebAPI/Controllers/Identity/RolesApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore9.WebAPI.Controllers.Identity
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesApiController : ControllerBase
+    {
+    }
+}
