4370fba35b62979aa76e9c9e2e6b62450f89a468
Rueslan <krus1501@mail.ru>
2025-03-01 13:59:07 +0400

    Пример расстановки ILogger<T>

--- UI/WebStore9/Controllers/AccountController.cs
+++ UI/WebStore9/Controllers/AccountController.cs
@@ -11,11 +11,13 @@ namespace WebStore9.Controllers
     {
         private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
-        
-        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager, ILogger<AccountController> logger)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _logger = logger;
         }
 
         #region Registration
@@ -29,21 +31,33 @@ namespace WebStore9.Controllers
         {
             if (!ModelState.IsValid) return View(model);
 
-            var user = new User { UserName = model.UserName };
-
-            var registerResult = await _userManager.CreateAsync(user, model.Password);
-            if (registerResult.Succeeded)
+            using (_logger.BeginScope("Регистрация пользователя {0}", model.UserName))
             {
-                await _signInManager.SignInAsync(user, false);
+                var user = new User { UserName = model.UserName };
 
-                await _userManager.AddToRoleAsync(user, Role.Users);
+                _logger.LogInformation("Регистрация пользователя {0}", user.UserName);
 
-                return RedirectToAction("Index", "Home");
+                var registerResult = await _userManager.CreateAsync(user, model.Password);
+                if (registerResult.Succeeded)
+                {
+                    _logger.LogInformation("Пользователь {0} успешно зарегистрирован", user.UserName);
+
+                    await _userManager.AddToRoleAsync(user, Role.Users);
+
+                    _logger.LogInformation("Пользователю {0} назначена роль {1}", user.UserName, Role.Users);
+
+                    await _signInManager.SignInAsync(user, false);
+
+                    _logger.LogInformation("Пользователь {0} вошёл в систему после регистрирации", user.UserName);
+
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach (var error in registerResult.Errors)
+                    ModelState.AddModelError("", error.Description);
+
+                _logger.LogWarning("Ошибка регистрации пользователя {0}: {1}", user.UserName, string.Join(", ", registerResult.Errors.Select(e => e.Description)));
             }
-
-            foreach (var error in registerResult.Errors)
-                ModelState.AddModelError("", error.Description);
-
 
             return View(model);
         }
@@ -59,7 +73,8 @@ namespace WebStore9.Controllers
         [AllowAnonymous]
         public async Task<IActionResult> Login(LoginViewModel model)
         {
-            if (!ModelState.IsValid) return View(model);
+            if (!ModelState.IsValid) 
+                return View(model);
 
             var loginResult = await _signInManager.PasswordSignInAsync(
                 model.UserName, 
@@ -68,9 +83,15 @@ namespace WebStore9.Controllers
                 false);
 
             if (loginResult.Succeeded)
+            {
+                _logger.LogInformation("Пользователь {0} успешно вошёл в систему", model.UserName);
                 return LocalRedirect(model.ReturnUrl);
+            }
 
             ModelState.AddModelError("", "Ошибка ввода имени пользователя или пароля");
+
+            _logger.LogWarning("Ошибка ввода имени пользователя или пароля при входе {0}", model.UserName);
+
             return View(model);
         }
 
@@ -78,11 +99,20 @@ namespace WebStore9.Controllers
 
         public async Task<IActionResult> Logout()
         {
+            var userName = User.Identity!.Name;
+
             await _signInManager.SignOutAsync();
+
+            _logger.LogInformation("Пользователь {0} вышел из системы", userName);
+
             return RedirectToAction("Index", "Home");
         }
 
         [AllowAnonymous]
-        public IActionResult AccessDenied() => View();
+        public IActionResult AccessDenied()
+        {
+            _logger.LogWarning("Отказано в доступе {0} к uri: {1}", User.Identity!.Name, HttpContext.Request.Path);
+            return View();
+        }
     }
 }
