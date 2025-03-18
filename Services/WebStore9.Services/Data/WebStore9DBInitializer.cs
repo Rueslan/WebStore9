9e002ebd4eb9a9e07b812659c39583619cdc9d7e
Rueslan <krus1501@mail.ru>
2025-03-18 19:23:48 +0400

    Добавление условия в инициализатор для поддержки инициализации данных в памяти

--- Services/WebStore9.Services/Data/WebStore9DBInitializer.cs
+++ Services/WebStore9.Services/Data/WebStore9DBInitializer.cs
@@ -116,16 +116,29 @@ namespace WebStore9.Services.Data
 
 
             _logger.LogInformation("Запись данных...");
-            await using (await _db.Database.BeginTransactionAsync())
+           
+            if (_db.Database.IsSqlServer())
+            {
+                await using (await _db.Database.BeginTransactionAsync())
+                {
+                    _db.Sections.AddRange(TestData.Sections);
+                    _db.Brands.AddRange(TestData.Brands);
+                    _db.Products.AddRange(TestData.Products);
+                    _db.Employees.AddRange(TestData.Employees);
+
+                    await _db.SaveChangesAsync();
+                    await _db.Database.CommitTransactionAsync();
+                }
+            }
+            else
             {
                 _db.Sections.AddRange(TestData.Sections);
                 _db.Brands.AddRange(TestData.Brands);
                 _db.Products.AddRange(TestData.Products);
                 _db.Employees.AddRange(TestData.Employees);
-
                 await _db.SaveChangesAsync();
-                await _db.Database.CommitTransactionAsync();
             }
+
             _logger.LogInformation("Запись данных выполнена успешно за {0} мс", timer.Elapsed.TotalMilliseconds);
 
         }
