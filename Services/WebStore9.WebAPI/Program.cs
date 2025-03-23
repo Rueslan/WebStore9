497578aa754b89f8776789cfc426e960d1383e10
Rueslan <krus1501@mail.ru>
2025-03-23 10:17:33 +0400

    рефакторинг

--- Services/WebStore9.WebAPI/Program.cs
+++ Services/WebStore9.WebAPI/Program.cs
@@ -86,19 +86,19 @@ namespace WebStore9.WebAPI
                     options.EnableAnnotations();
                     options.SwaggerDoc("v1", new OpenApiInfo { Title = "WebStore9 API", Version = "v1" });
 
-                    const string webstore_api_xml = "WebStore9.WebAPI.xml";
-                    const string webstore_domain_xml = "WebStore9.Domain.xml";
-                    const string debug_path = "bin/debug/net9.0";
+                    const string webstoreApiXml = "WebStore9.WebAPI.xml";
+                    const string webstoreDomainXml = "WebStore9.Domain.xml";
+                    const string debugPath = "bin/debug/net9.0";
 
-                    if (File.Exists(webstore_api_xml))
-                        options.IncludeXmlComments(webstore_api_xml);
-                    else if (File.Exists(Path.Combine(debug_path, webstore_api_xml)))
-                        options.IncludeXmlComments(Path.Combine(debug_path, webstore_api_xml));
+                    if (File.Exists(webstoreApiXml))
+                        options.IncludeXmlComments(webstoreApiXml);
+                    else if (File.Exists(Path.Combine(debugPath, webstoreApiXml)))
+                        options.IncludeXmlComments(Path.Combine(debugPath, webstoreApiXml));
 
-                    if (File.Exists(webstore_domain_xml))
-                        options.IncludeXmlComments(webstore_domain_xml);
-                    else if (File.Exists(Path.Combine(debug_path, webstore_domain_xml)))
-                        options.IncludeXmlComments(Path.Combine(debug_path, webstore_domain_xml));
+                    if (File.Exists(webstoreDomainXml))
+                        options.IncludeXmlComments(webstoreDomainXml);
+                    else if (File.Exists(Path.Combine(debugPath, webstoreDomainXml)))
+                        options.IncludeXmlComments(Path.Combine(debugPath, webstoreDomainXml));
                 });
 
             builder.Services.AddEndpointsApiExplorer();
