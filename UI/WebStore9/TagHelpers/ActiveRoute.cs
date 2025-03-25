11de7e16b6138e25846165e8bf9fd1fadcb68f88
Rueslan <krus1501@mail.ru>
2025-03-25 13:47:07 +0400

    Подготовка данных внутри логики таг-хелпера

--- UI/WebStore9/TagHelpers/ActiveRoute.cs
+++ UI/WebStore9/TagHelpers/ActiveRoute.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace WebStore9.TagHelpers
 {
@@ -6,8 +8,23 @@ namespace WebStore9.TagHelpers
     public class ActiveRoute : TagHelper
     {
         private const string AttributeName = "ws-is-active-route";
+
+        [HtmlAttributeName("asp-controller")]
+        public string Controller { get; set; }
+
+        [HtmlAttributeName("asp-action")]
+        public string Action { get; set; }
+
+        [HtmlAttributeName("asp-all-route-data", DictionaryAttributePrefix = "asp-route-")]
+        public Dictionary<string, string> RouteValuesDictionary { get; set; } = new(StringComparer.OrdinalIgnoreCase);
+
+        [ViewContext, HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
+
+
             output.Attributes.RemoveAll(AttributeName);
         }
     }
