d7a6f0e07ca6c9d40423af73db6bf60199a8027d
Rueslan <krus1501@mail.ru>
2025-03-25 14:12:59 +0400

    Основная логика таг-хелпера

--- UI/WebStore9/TagHelpers/ActiveRoute.cs
+++ UI/WebStore9/TagHelpers/ActiveRoute.cs
@@ -23,9 +23,48 @@ namespace WebStore9.TagHelpers
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-
+            if (IsActive())
+                MakeActive(output);
 
             output.Attributes.RemoveAll(AttributeName);
+        }
+
+        private bool IsActive()
+        {
+            var routeValues = ViewContext.RouteData.Values;
+            var routeController = routeValues["controller"]?.ToString();
+            var routeAction = routeValues["action"]?.ToString();
+
+            if (Action is { Length: > 0 } action && !string.Equals(action, routeAction))
+                return false;
+
+            if (Controller is { Length: > 0 } controller && !string.Equals(controller, routeController))
+                return false;
+
+            foreach (var (key, value) in RouteValuesDictionary)
+            {
+                if (!routeValues.ContainsKey(key) || routeValues[key]?.ToString() != value)
+                    return false;
+            }
+
+            return true;
+        }
+
+        private void MakeActive(TagHelperOutput output)
+        {
+            var classAttribute = output.Attributes.FirstOrDefault(atr => atr.Name == "class");
+
+            if (classAttribute is null)
+            {
+               output.Attributes.Add("class","active"); 
+            }
+            else
+            {
+                if (classAttribute.Value?.ToString().Contains("active") ?? false)
+                    return;
+
+                output.Attributes.SetAttribute("class", classAttribute.Value + " active");
+            }
         }
     }
 }
